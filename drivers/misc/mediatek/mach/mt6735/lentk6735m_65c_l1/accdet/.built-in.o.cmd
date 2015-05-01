ef7526de64a28b8171ac92eaa9afaab22e974740
Takashi Iwai <tiwai@suse.de>
2015-05-01 09:20:34 +0200

    f
    Former-commit-id: b442e23a0e82a84c77810c1b2c559600d52b3c44

--- drivers/misc/mediatek/mach/mt6735/lentk6735m_65c_l1/accdet/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/misc/mediatek/mach/mt6735/lentk6735m_65c_l1/accdet/built-in.o :=  /home/olegsvs/Android/aarch64-linux-android-4.9/bin/aarch64-linux-android-ld -EL    -r -o drivers/misc/mediatek/mach/mt6735/lentk6735m_65c_l1/accdet/built-in.o drivers/misc/mediatek/mach/mt6735/lentk6735m_65c_l1/accdet/accdet_custom.o 
