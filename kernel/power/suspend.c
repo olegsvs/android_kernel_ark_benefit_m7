11b425cfca648bf4ed1eb8815faad9a3fd4b07dd
Stefan Guendhoer <stefan@guendhoer.com>
2016-01-07 22:23:23 +0000

    fdgdfg
    Former-commit-id: 7c513c2da9814be5b3ddc7539d07f4e6f1fdf6fd

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -400,6 +400,10 @@ int enter_state(suspend_state_t state)
 
 	if (state == PM_SUSPEND_FREEZE)
 		freeze_begin();
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7e69c4e... power: make sync on suspend optional
 #ifdef CONFIG_PM_SYNC_BEFORE_SUSPEND
 	printk(KERN_INFO "PM: Syncing filesystems ... ");
 #if MTK_SOLUTION
@@ -413,6 +417,10 @@ int enter_state(suspend_state_t state)
 #endif
 	printk("done.\n");
 #endif
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7e69c4e... power: make sync on suspend optional
 	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state].label);
 	error = suspend_prepare(state);
 	if (error)
