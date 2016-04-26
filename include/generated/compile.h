7cc9c2fcdfdf73304872a7dd50d4400401ea778d
olegsvs <oleg.texet@gmail.com>
2016-04-26 22:53:44 +0300

    Vol+ Vol- Batt fix step 1
    Former-commit-id: aeb3fb343f39134e96de47535854ebb884a39208

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 8 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#6 SMP PREEMPT Tue Apr 26 00:11:50 MSK 2016"
+#define UTS_VERSION "#8 SMP PREEMPT Tue Apr 26 22:47:51 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
