levincalado/cody_kernel_sprout

pocket_mod: sysfs: enable: sysfs can't be world writable
Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

drivers/misc/pocket_mod.c
@@ -97,7 +97,7 @@ static ssize_t pocket_mod_set(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(enable, 0777,
+static DEVICE_ATTR(enable, (S_IWUSR|S_IRUGO),
 		pocket_mod_show, pocket_mod_set);
 
 static ssize_t pocket_mod_timeout_show(struct device *dev,

olegsvs/android_kernel_ark_benefit_m7

Signed-off-by: Stefan Guendhoer <stefan@guendhoer.com>
Former-commit-id: f5be997857e1cd65ee003b12da86f92b5f7b1f6f

drivers/misc/pocket_mod.c
@@ -74,7 +74,7 @@ static ssize_t pocket_mod_set(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(pocket_mod_enable, 0777,
+static DEVICE_ATTR(pocket_mod_enable, (S_IWUSR|S_IRUGO),
 		pocket_mod_show, pocket_mod_set);
 
 static struct attribute *pocket_mod_attributes[] =