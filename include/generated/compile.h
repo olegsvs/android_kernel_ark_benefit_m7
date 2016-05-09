f77aa01c49650228f038cc29b6d7e887816ea6ad
olegsvs <oleg.texet@gmail.com>
2016-05-09 22:09:57 +0300

    rm68200 ver0.3_cpt

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#9 SMP PREEMPT Mon May 9 22:07:12 MSK 2016"
+#define UTS_VERSION "#10 SMP PREEMPT Mon May 9 22:08:52 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
