dcf0e0609c935ddb2602db040eb8bf6fd16b260f
olegsvs <oleg.texet@gmail.com>
2016-05-10 21:34:54 +0300

    split defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#15 SMP PREEMPT Tue May 10 21:23:38 MSK 2016"
+#define UTS_VERSION "#16 SMP PREEMPT Tue May 10 21:27:40 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
