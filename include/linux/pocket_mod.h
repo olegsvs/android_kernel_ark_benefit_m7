185e1e3932bf08fde379b3bc8c9f533ba6229bf1
olegsvs <oleg.texet@gmail.com>
2016-05-10 21:49:01 +0300

    fix typo

--- include/linux/pocket_mod.h
+++ include/linux/pocket_mod.h
@@ -6,7 +6,7 @@ extern int is_screen_on;
 #ifdef CONFIG_MTK_STK3X1X_DRIVER
 int stk3x1x_pocket_detection_check(void);
 #endif
-#ifdef CONFIG_MTK_epl2182_NEW
+#ifdef CONFIG_MTK_EPL2182_NEW
 int epl2182_pocket_detection_check(void);
 #endif
 int device_is_pocketed(void);
