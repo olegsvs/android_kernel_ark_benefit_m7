29c860ac9f2016ae2738096b728fd7529ddb74e0
olegsvs <oleg.texet@gmail.com>
2016-05-10 22:48:29 +0300

    JD9367 test inversion

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 20 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#19 SMP PREEMPT Tue May 10 22:38:57 MSK 2016"
+#define UTS_VERSION "#20 SMP PREEMPT Tue May 10 22:46:34 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
