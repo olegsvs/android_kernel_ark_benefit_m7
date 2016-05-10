84987f13d68517e30eb0f04ad90f1eea4ab70ab8
olegsvs <oleg.texet@gmail.com>
2016-05-11 00:06:48 +0300

    fix defconfigs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 29 */
+/* This file is auto generated, version 32 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#29 SMP PREEMPT Tue May 10 23:44:33 MSK 2016"
+#define UTS_VERSION "#32 SMP PREEMPT Wed May 11 00:04:10 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
