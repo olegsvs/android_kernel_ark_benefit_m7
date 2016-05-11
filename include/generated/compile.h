dd67387bcf2a8a633258972782db9ae5c9e16656
olegsvs <oleg.texet@gmail.com>
2016-05-11 22:48:54 +0300

    Revert "JD9365 ver8_3_3"
    This reverts commit f524d465e0c04414188361b28d1424a3938cba33.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 43 */
+/* This file is auto generated, version 42 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#43 SMP PREEMPT Wed May 11 22:47:50 MSK 2016"
+#define UTS_VERSION "#42 SMP PREEMPT Wed May 11 22:46:19 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
