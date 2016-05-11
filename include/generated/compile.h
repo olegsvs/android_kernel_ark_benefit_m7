046a13582e6663aab965bdfc278c1587be199cb3
olegsvs <oleg.texet@gmail.com>
2016-05-11 23:48:08 +0300

    Revert "touch vibration button"
    This reverts commit e1d9e2d6a452c6403ad9e475d2ca7c369049ca45.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 51 */
+/* This file is auto generated, version 46 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#51 SMP PREEMPT Wed May 11 23:45:06 MSK 2016"
+#define UTS_VERSION "#46 SMP PREEMPT Wed May 11 22:52:32 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
