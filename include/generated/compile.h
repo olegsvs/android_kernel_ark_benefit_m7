649326f91ab9db54aa867efdaf496e41ebbebac6
olegsvs <oleg.texet@gmail.com>
2016-05-11 22:41:19 +0300

    JD9365 ver8_3_1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 37 */
+/* This file is auto generated, version 40 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#37 SMP PREEMPT Wed May 11 00:31:27 MSK 2016"
+#define UTS_VERSION "#40 SMP PREEMPT Wed May 11 02:12:24 MSK 2016"
 #define LINUX_COMPILE_BY "olegsvs"
 #define LINUX_COMPILE_HOST "olegsvs-develop"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
