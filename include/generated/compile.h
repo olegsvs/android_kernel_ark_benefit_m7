a3f787bf937f7919b80b37d3729808bd229107ae
olegsvs <oleg.texet@gmail.com>
2016-05-13 22:00:08 +0300

    git clean [2/4]

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 54 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#54 SMP PREEMPT Wed May 11 23:51:46 MSK 2016"
-#define LINUX_COMPILE_BY "olegsvs"
-#define LINUX_COMPILE_HOST "olegsvs-develop"
-#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
